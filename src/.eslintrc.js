dd30186826b504eca725374eee34c947e5412219
Khac Man <khacmanle@gmail.com>
2017-12-21 23:40:22 +0700

    Init msg-auth

---
+++ src/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": 0
+    }
+};
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function getProfile(req, res, next) {
+    res.send("respond with a resource");
+}
+
+module.exports = {
+    getProfile
+};
